71cc48cbe39d49cdbbbb04bc050166cf75c18fdf
jillur.rah <jillur.cse.bd@gmail.com>
2022-12-21 11:51:03 +0600

    conditional rendering

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CONDITIONAL_RENDERING from './CONDITIONAL_RENDERING'
+
+
+export default function App() {
+  return (
+    <div>
+   <CONDITIONAL_RENDERING />
+    </div>
+  )
+}
+
